d9f4aaa93dd8fd88c5705f1325096f7c66d630fd
Phan Thi Ngoc Vy <18dh110199@st.huflit.edu.vn>
2020-05-13 04:12:19 +0700

    hello

---
+++ Bai1/NgocVy/buoi6/DataProvider.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi6
+{
+    class DataProvider
+    {
+        public static readonly string connectionString = "Data Source=DESKTOP-G3K9KPG;Initial Catalog=QLQuan;Integrated Security=True";
+
+        SqlConnection connection;
+        public void ListProduct()
+        {
+            connection = new SqlConnection(connectionString);
+            string queryString = "SELECT * FROM ThucUong;";
+            SqlCommand command = new SqlCommand(queryString, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("\t{0}\t{1}\t{2}",
+                        reader[0], reader[1], reader[2]);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+        public void AddProduct(string MSHH, string TenHang, int Gia, int TinhTrang)
+        {
+            connection = new SqlConnection(connectionString);
+            string queryString = "INSERT INTO ThucUong (MSHH, TenHang, Gia, TinhTrang)" + "VALUES (@MSHH, @TenHang, @Gia, @TinhTrang);";
+
+            string mshh = MSHH;
+            string tenHang = TenHang;
+            int gia = Gia;
+            int tinhTrang = TinhTrang;
+
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@MSHH", mshh);
+            command.Parameters.AddWithValue("@TenHang", tenHang);
+            command.Parameters.AddWithValue("@Gia", gia);
+            command.Parameters.AddWithValue("@TinhTrang", tinhTrang);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+        }
+
+        public void DeleteProduct(string MSHH)
+        {
+            connection = new SqlConnection(connectionString);
+            string queryString = "DELETE FROM ThucUong WHERE MSHH=@MSHH";
+
+            string mshh = MSHH;
+
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@MSHH", mshh);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+        }
+
+        public void RepairProduct(string MSHH)
+        {
+            connection = new SqlConnection(connectionString);
+            string queryString = "UPDATE ThucUong SET TenHang = @TenHang, Gia= @Gia WHERE MSHH=@MSHH";
+
+            string mshh = MSHH;
+            Console.Write("Nhap Ten Hang Hoa Can Sua: ");
+            string tenHang = Console.ReadLine();
+            Console.Write("Nhap Gia Can Sua: ");
+            string gia = Console.ReadLine();
+
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@MSHH", mshh);
+            command.Parameters.AddWithValue("@TenHang", tenHang);
+            command.Parameters.AddWithValue("@Gia", gia);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+        }
+
+        public void ListBill()
+        {
+            connection = new SqlConnection(connectionString);
+            string queryString = "SELECT * FROM HoaDon;";
+            SqlCommand command = new SqlCommand(queryString, connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("\t{0}\t{1}",
+                    reader[0], reader[1]);
+            }
+            reader.Close();
+        }
+
+        public void CreateBill(string MSDH, string dateTime)
+        {
+            connection = new SqlConnection(connectionString);
+            string queryString = "INSERT INTO HoaDon (MSDH, NgayDat) VALUES (@MSDH, @NgayDat);";
+
+            string msdh = MSDH;
+            string ngayDat = dateTime;
+
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@MSDH", msdh);
+            command.Parameters.AddWithValue("@NgayDat", ngayDat);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+        }
+
+        public void CreateDetailBill(string MSHD, string MSHH, int SoLuong, int TiLeGiam)
+        {
+            connection = new SqlConnection(connectionString);
+            string queryString = "INSERT INTO HDChiTietDatHang (MSHD, MSHH, SoLuong, TiLeGiam) VALUES (@MSHD, @MSHH, @SoLuong, @TiLeGiam);";
+
+
+            string mshh = MSHH;
+            string mshd = MSHD;
+            int soLuong = SoLuong;
+            int tiLeGiam = TiLeGiam;
+
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@MSHD", mshd);
+            command.Parameters.AddWithValue("@MSHH", mshh);
+            command.Parameters.AddWithValue("@SoLuong", soLuong);
+            command.Parameters.AddWithValue("@TiLeGiam", tiLeGiam);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+        }
+
+        public void ListDetail()
+        {
+            connection = new SqlConnection(connectionString);
+            string queryString = "SELECT * FROM HDChiTietDatHang;";
+            SqlCommand command = new SqlCommand(queryString, connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("\t{0}\t{1}\t{2}\t{3}",
+                    reader[0], reader[1], reader[2], reader[3]);
+            }
+            reader.Close();
+        }
+    }
+}
---
+++ Bai1/NgocVy/buoi6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi6
+{
+    class Program
+    {
+        static void Add()
+        {
+            DataProvider Data = new DataProvider();
+            Console.Write("Nhap ma so hang hoa: ");
+            string mshh = Console.ReadLine();
+            Console.Write("Nhap ten Hang: ");
+            string tenHang = Console.ReadLine();
+            Console.Write("Nhap gia: ");
+            int gia = int.Parse(Console.ReadLine());
+            Console.Write("Nhap tinh trang: ");
+            int tinhTrang = int.Parse(Console.ReadLine());
+            Data.AddProduct(mshh, tenHang, gia, tinhTrang);
+            Data.ListProduct();
+        }
+        static void Delete()
+        {
+            DataProvider Data = new DataProvider();
+            Console.Write("Nhap ma so hang hoa can xoa: ");
+            string mshh = Console.ReadLine();
+            Data.DeleteProduct(mshh);
+            Data.ListProduct();
+        }
+        static void Repair()
+        {
+            DataProvider Data = new DataProvider();
+            Console.Write("Nhap ma so hang hoa can sua: ");
+            string mshh = Console.ReadLine();
+            Data.RepairProduct(mshh);
+            Data.ListProduct();
+        }
+
+        static void AddBill()
+        {
+            DataProvider Data = new DataProvider();
+            Console.Write("Nhap ma so hoa don: ");
+            string msdh = Console.ReadLine();
+            Data.CreateBill(msdh, "08/05/2020");
+            Data.ListBill();
+        }
+        static void Main(string[] args)
+        {
+            DataProvider Data = new DataProvider();
+            Console.WriteLine("DANH SACH CAC THUC UONG");
+            Data.ListProduct();
+            Console.WriteLine("-------------------------------------");
+            Console.Write("Nhap ma so hoa don: ");
+            string msdh = Console.ReadLine();
+            Console.Write("Vui long nhap ma so thuc uong ban muon order: ");
+            string mshh = Console.ReadLine();
+            Console.Write("Vui long nhap so luong ban muon order: ");
+            int soLuong = int.Parse(Console.ReadLine());
+            Console.Write("Ti le giam cua sam pham ban order: ");
+            int tiLeGiam = int.Parse(Console.ReadLine());
+
+            Data.CreateDetailBill(msdh, mshh, soLuong, tiLeGiam);
+            Data.ListDetail();
+            Console.ReadKey();
+        }
+    }
+}
